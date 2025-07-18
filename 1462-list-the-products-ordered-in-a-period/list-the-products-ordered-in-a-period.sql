204d9fb12ff585b718ac06bcc5d1b7fdb4b3c092
Palak <malikpalak1332@gmail.com>
2025-07-19 01:28:58 +0530

    Time: 960 ms (9.44%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1462-list-the-products-ordered-in-a-period/list-the-products-ordered-in-a-period.sql
@@ -0,0 +1,2 @@
+# Write your MySQL query statement below
+select Products.product_name ,sum(Orders.unit) as unit from Products,Orders where Orders.product_id=Products.product_id and Orders.order_date between '2020-02-01' and '2020-02-29' group by Products.product_name,Orders.product_id having sum(Orders.unit)>=100;
